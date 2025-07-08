4cc70f57f2c6ca383f10f178ff20f4c4687da062
Bilal Aqeel <sheikhbilal1025@gmail.com>
2025-07-08 19:43:56 +0500

    Solve Local Storage Issue

--- js/script.js
+++ js/script.js
@@ -15,19 +15,19 @@ const app = Vue.createApp({
     deleteTask(index) {
       this.Tasks.splice(index, 1);
     },
-    mounted() {
-      const saved = localStorage.getItem("tasks");
-      if (saved) {
-        this.Tasks = JSON.parse(saved);
-      }
-    },
-    watch: {
-      Tasks: {
-        handler(newVal) {
-          localStorage.setItem("tasks", JSON.stringify(newVal));
-        },
-        deep: true,
+  },
+  mounted() {
+    const saved = localStorage.getItem("Tasks");
+    if (saved) {
+      this.Tasks = JSON.parse(saved);
+    }
+  },
+  watch: {
+    Tasks: {
+      handler(newVal) {
+        localStorage.setItem("Tasks", JSON.stringify(newVal));
       },
+      deep: true,
     },
   },
 });
