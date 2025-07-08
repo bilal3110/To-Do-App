16634c6b8337d1fa58da89b3c53d17a8f34754a0
Bilal Aqeel <sheikhbilal1025@gmail.com>
2025-07-08 19:35:50 +0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      newTask: "",
+      Tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.Tasks.push(this.newTask.trim());
+        this.newTask = "";
+      }
+    },
+    deleteTask(index) {
+      this.Tasks.splice(index, 1);
+    },
+    mounted() {
+      const saved = localStorage.getItem("tasks");
+      if (saved) {
+        this.Tasks = JSON.parse(saved);
+      }
+    },
+    watch: {
+      Tasks: {
+        handler(newVal) {
+          localStorage.setItem("tasks", JSON.stringify(newVal));
+        },
+        deep: true,
+      },
+    },
+  },
+});
+
+app.mount("#app");
